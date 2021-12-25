09e1984a945b4bcc7684f769710b9deed7e4afac
roadblock37 <Roadblock37@gmail.com>
2021-12-24 17:21:00 -0800

    added tracklist component in render

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchResults.css';
-import {TrackList} from '../Tracklist/Tracklist';
+import {Tracklist} from '../Tracklist/Tracklist';
 
 export class SearchResults extends React.Component{
     render(){
         return (
             <div className="SearchResults">
   <h2>Results</h2>
-   {/*Add a TrackList component*/}
+   <Tracklist/>
 </div>
         );
     }
